3acecfd7d713dbbcadac12123ae0588269db41c3
briosoemilio <brioso.emilioluis@gmail.com>
2024-10-13 17:51:17 +0800

    Add ability tool tip

--- app/_components/AbilityIcon.jsx
+++ app/_components/AbilityIcon.jsx
@@ -1,10 +1,14 @@
 'use client'
 import React from 'react'
 import { capitalize } from 'lodash'
+import useFetchAbilityInfo from '../pokemon/useFetchAbilityInfo'
 
 const AbilityIcon = ({ ability }) => {
+  const { abilityInfo } = useFetchAbilityInfo(ability.name)
+  const effectDetails = abilityInfo?.['short_effect'] || abilityInfo?.effect
+
   return (
-    <div className={`flex flex-row mx-3 border-white border-2 p-1 rounded-md`}>
+    <div className={`tooltip flex flex-row mx-3 border-white border-2 p-1 rounded-md`} data-tip={effectDetails}>
       <p className='text-white'>
         {capitalize(ability.name)}
       </p>
