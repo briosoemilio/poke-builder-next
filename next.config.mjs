52ea2c5324d4d425810aafde829838d402442926
briosoemilio <brioso.emilioluis@gmail.com>
2024-10-13 12:43:27 +0800

    Add wordpress base url to env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_WP_BASE_URL: process.env.NEXT_PUBLIC_WP_BASE_URL
+  }
+};
 
 export default nextConfig;
